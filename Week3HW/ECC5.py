8339b5467d1b9b012810db8a47521335514b11f4
timgruen <tgruenha@iu.edu>
2020-01-30 18:34:23 -0500

    added

---
+++ Week3HW/ECC5.py
@@ -0,0 +1,17 @@
+from cloudmesh.common.StopWatch import StopWatch
+
+class ECC5:
+
+    def __init__(self):
+        self.start = input('Press Enter to start')
+        StopWatch.start('test')
+        self.stop = input('Press Enter to stop')
+        StopWatch.stop('test')
+        self.time = StopWatch.get('test')
+
+    def run(self):
+        print(self.time,' second(s) have passed.')
+
+if __name__ == '__main__':
+    root = ECC5()
+    root.run()
