1e45cdf4f90cdabe73d89c28f38947aced238e1e
timgruen <tgruenha@iu.edu>
2020-01-30 16:52:31 -0500

    added

---
+++ Week3HW/ECC1.py
@@ -0,0 +1,30 @@
+from cloudmesh.common.util import banner, HEADING
+from cloudmesh.common.debug import VERBOSE
+from cloudmesh.common.variables import Variables
+
+variables = Variables()
+
+variables['debug'] = True
+variables['trace'] = True
+variables['verbose'] = 10
+banner("hello")
+
+
+class ECC1:
+
+    dictexample = {"One":"Uno","Two":"Dos","Three":"Tres"}
+
+    def __init__(self,ban):
+        self.ban = ban
+
+
+    def run(self):
+        banner(self.ban)
+        HEADING(self.ban+"\nThis Banner is special, it tells you what function printed it!")
+        print("Below is an example dictionary:\n")
+        VERBOSE(self.dictexample)
+
+if __name__ == "__main__":
+    root = ECC1(input("What would you like as your banner?\n"))
+    root.run()
+
